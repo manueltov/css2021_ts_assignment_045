243ca6d0607f7c32d6c41dfcf23c28e022aca721
Eduardo Salvadinha <eduardo.salvadinha@outlook.pt>
2021-03-11 08:19:51 +0000

    Other project deleted

--- src/main/java/client/SimpleClient.java
+++ src/main/java/client/SimpleClient.java
@@ -3,9 +3,7 @@ package client;
 import java.time.LocalDate;
 
 import facade.exceptions.ApplicationException;
-import facade.services.CustomerService;
 import facade.services.EventService;
-import facade.services.SaleService;
 
 /**
  * A simple application client that uses both services.
@@ -27,9 +25,8 @@ public final class SimpleClient {
 	 */
 	public static void main(String[] args) {
 
-		// Access both available services
-		CustomerService cs = CustomerService.INSTANCE;
-		SaleService ss = SaleService.INSTANCE;		
+		
+		//TESTE
 		EventService es = EventService.INSTANCE;
 		
 		try {
@@ -40,30 +37,5 @@ public final class SimpleClient {
 		}
 	
 		
-		
-		// the interaction
-		try {
-			// adds a customer.
-			cs.addCustomer(168027852, "Customer 1", 217500255, 2);
-
-			// creates a new sale
-			int sale = ss.newSale(168027852);
-
-			// adds two products to the sale
-			ss.addProductToSale(sale, 123, 10);
-			ss.addProductToSale(sale, 124, 5);
-			
-			// gets the discount amounts
-			double discount = ss.getSaleDiscount(sale);
-			System.out.println(discount);
-		} catch (ApplicationException e) {
-			System.err.println("Error: " + e.getMessage());
-			// for debugging purposes only. Typically, in the application
-			// this information can be associated with a "details" button when
-			// the error message is displayed.
-			if (e.getCause() != null) 
-				System.err.println("Cause: ");
-			e.printStackTrace();
-		}
 	}
 }
